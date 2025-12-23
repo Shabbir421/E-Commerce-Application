0e064aa08296d4ff3f60154eecc4123b2e892fb8
VishalKushwaha123 <vishal818799@gmail.com>
2025-12-23 13:11:17 +0530

    app

---
+++ backend/configs/mongodb.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+// connect to mongoDb database
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () =>
+    console.log("Database connected successfully!")
+  );
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "Edemy_courses",
+  });
+};
+export default connectDB;
