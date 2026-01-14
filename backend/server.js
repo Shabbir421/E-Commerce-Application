40743b764df6cb033a821cc4ee8e5ce50f7dbed9
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-15 00:11:43 +0530

    cc

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,7 @@ import { clerkMiddleware } from "@clerk/express";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 
 const app = express();
-// Webhook route FIRST
-app.post("/clerk", express.raw({ type: "application/json" }), clerkWebhooks);
 
-// Other middleware AFTER
-app.use(express.json());
 // Normal middleware
 app.use(cors());
 app.use(clerkMiddleware());
@@ -24,6 +20,8 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => {
   res.send("API is working fine!");
 });
+// Clerk webhooks route
+app.post("/clerk", express.json(), clerkWebhooks);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
