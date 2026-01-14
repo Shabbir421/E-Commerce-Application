3e1c13af5e2fb05c5b532f02fdeda18b4ccff6ef
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-14 21:40:21 +0530

    backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,23 @@
 /** @format */
 
 import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+} from "@clerk/clerk-react";
 
 const App = () => {
   return (
     <div className="text-default min-h-screen bg-white">
-      <h1>hello</h1>
+      <h1 className="text-3xl font-bold underline">Hello, Admin Panel!</h1>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 };
--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import Path from "path";
 import connectDB from "./configs/mongodb.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 // initialize express
 const app = express();
 const __dirname = Path.resolve();
 
-// connect to db
-await connectDB();
-
 // middleware
 app.use(cors());
+app.use(clerkMiddleware());
 
 // Routes
 app.get("/api", (req, res) => {
   res.send(" API is working fine!");
 });
+
+//!Routes
+app.post("/clerk", express.json(), clerkWebhooks);
 
 // production setup
 if (process.env.NODE_ENV === "production") {
@@ -35,5 +39,7 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
+  connectCloudinary();
   console.log(`Server is running on ${PORT}`);
 });
