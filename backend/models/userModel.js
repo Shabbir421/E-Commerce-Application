c014643aad3605a6598d353b798585a9550a82ee
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-15 15:50:59 +0530

    cc

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,41 +1,15 @@
 /** @format */
-
-// /** @format */
-
-// import mongoose from "mongoose";
-
-// const addressSchema = new mongoose.Schema({
-//   label: { type: String, required: true },
-//   fullName: { type: String, required: true },
-//   phoneNumber: { type: String, required: true },
-//   streetAddress: { type: String, required: true },
-//   city: { type: String, required: true },
-//   state: { type: String, required: true },
-//   zipCode: { type: String, required: true },
-//   isDefault: { type: Boolean, default: false },
-// });
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     clerkId: {
-//       type: String,
-//       required: true,
-//     },
-//     name: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     imageUrl: { type: String, default: "" },
-//     addresses: [addressSchema],
-//     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-//   },
-//   { timestamps: true }
-// );
-
-// const User = mongoose.model("User", userSchema);
-// export default User;
-
-/** @format */
-
 import mongoose from "mongoose";
+const addressSchema = new mongoose.Schema({
+  label: { type: String, required: true },
+  fullName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  streetAddress: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  isDefault: { type: Boolean, default: false },
+});
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,12 +17,8 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     imageUrl: { type: String, required: true },
-    enrolledCourses: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-      },
-    ],
+    addresses: [addressSchema],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
   { timestamps: true }
 );
