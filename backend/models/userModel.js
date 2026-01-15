5b5390d83680cedf14da277a45ef816b509cb657
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-15 15:58:30 +0530

    c

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,25 @@
 /** @format */
 import mongoose from "mongoose";
 
-const addressSchema = new mongoose.Schema({
-  label: { type: String, required: true },
-  fullName: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  streetAddress: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zipCode: { type: String, required: true },
-  isDefault: { type: Boolean, default: false },
-});
-
 const userSchema = new mongoose.Schema(
   {
     clerkId: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
     imageUrl: { type: String, required: true },
-    addresses: [addressSchema],
-    wishlist: [{ type: String, ref: "Product" }],
+    addresses: [
+      {
+        label: { type: String, required: true },
+        fullName: { type: String, required: true },
+        phoneNumber: { type: String, required: true },
+        streetAddress: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        zipCode: { type: String, required: true },
+        isDefault: { type: Boolean, default: false },
+      },
+    ],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
   { timestamps: true }
 );
