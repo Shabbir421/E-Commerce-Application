816eb05e2637d62e7f9f42f6967cfc9f3c6ea7a0
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-22 18:48:48 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,4 @@ app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoutes);
 
 //! port
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+export default app;
