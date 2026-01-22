8c9b483d81c9d304d26887b3cf71826f908d9c85
VishalKushwaha123 <vishal818799@gmail.com>
2026-01-22 17:05:28 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(
 );
 
 //! middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://e-commerce-application-adminpage.vercel.app", credentials: true }));
 app.use(clerkMiddleware());
 
 //! Routes
